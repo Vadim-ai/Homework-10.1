f2e67988e9a91332b8cfdb96d3aadd42e48082b5
SkyPro <vadim.phobia@gmail.com>
2022-05-25 00:04:16 +0300

    Выполнил ДЗ №10 + 1 сложное задание

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,41 @@ public class Main {
 
             Book[] books = new Book[5];
 
+            Author author1 = new Author("Джоан","Роулинг");
+            Author author2 = new Author("Джон", "Толкин");
+            Author author3 = new Author("Stephen", "King");
 
-            Author rouling = new Author("Джоан","Роулинг");
-            Author tolkin = new Author("Джон", "Толкин");
-
-            Book harryPotter1 = new Book("Гарри Поттер и Философский Камень", 1995, rouling);
-            Book theLordOfRings1 = new Book("Властелин Колец", 1954, tolkin);
-            Book harryPotter2 = new Book("Гарри Поттер и Тайная комната", 2000, rouling);
+            Book book0 = new Book("Гарри Поттер и Философский Камень", 1995, author1);
+            Book book1 = new Book("Властелин Колец", 1954, author2);
+            Book book2 = new Book("Гарри Поттер и Тайная комната", 2000, author1);
+            Book book3 = new Book("The Stand",1978,author3);
 
 
-            harryPotter1.setYearOfPublication(1994); // заменил год
-            System.out.println("Книга : " + harryPotter1.getNameOfBook() + ". Год издания :" + harryPotter1.getYearOfPublication()); //тестил
+            book0.setYearOfPublication(1994); // заменил год
+            AddNewBook(book0,books);
+            AddNewBook(book1,books);
+            AddNewBook(book2,books);
+            AddNewBook(book3,books);
+            PrintAllBooks(books);
 
-            System.out.println(harryPotter1.toString());
 
         }
+
+        public static void AddNewBook(Book book, Book[] books){
+            for (int i = 0; i < books.length; i++) {
+               if(books[i] == null){
+                   books[i] = book;
+                   break;
+               }
+            }
+        }
+
+    public static void PrintAllBooks(Book[]books) {
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] != null) {
+                System.out.println(books[i].toString());
+            }
+        }
+    }
+
     }
